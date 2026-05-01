df8552cc9ed883688c2e84aee3e37b90e687bd88
Paul Jensen <paulbjensen@gmail.com>
2026-05-02 00:02:38 +0200

    Switched to Cloudflare pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+
 import { defineConfig } from 'vite'
+import { cloudflare } from "@cloudflare/vite-plugin";
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [cloudflare(), svelte()],
 })
